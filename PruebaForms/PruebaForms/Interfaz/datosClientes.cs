dd2eb9e48f62fe0054cdd66fefa8fc7b5f67256a
Andrés Perachino <73505716+andresperachino8@users.noreply.github.com>
2024-08-16 00:32:29 -0300

    Add files via upload

---
+++ PruebaForms/PruebaForms/Interfaz/datosClientes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaForms.Interfaz
+{
+    public partial class datosClientes : Form
+    {
+        public datosClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void datosClientes_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                dataSet.ReadXml(@"..\\PruebaForms\\PruebaForms\\Repositorio\\datosClientes.xml");
+                dgvClientes.DataSource = dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar archivo XML de Clientes: " + ex.Message);
+            }
+        }
+    }
+}
